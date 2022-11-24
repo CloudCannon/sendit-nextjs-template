82a4c2f8c6ef5183ba79db5cabc2469131c33418
Jonatah <jonatah@cloudcannon.com>
2022-11-24 15:47:41 +1300

    Log react connector

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,11 @@ import '../styles/theme.scss';
 import {CloudCannonConnect} from '@cloudcannon/react-connector'
 
 export default function App({ Component, pageProps }) {
-	const AppComponent = CloudCannonConnect(Component);
+	const AppComponent = CloudCannonConnect(Component, {
+		processProps: (props) => {
+			console.log(props)
+			return props;
+		}
+	});
 	return <AppComponent {...pageProps}/>
 }
